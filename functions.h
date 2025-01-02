2c337fada2230dec80d4356eb47628932abea4a0
luudoanhai <hluudoan@gmail.com>
2025-01-02 19:40:33 +0700

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,18 @@
+#include "Datatypes.h"
+ 
+void inputStudent(Student students[], int *n);
+
+void printStudent(Student students[], int n);
+
+void addStudent(Student students[], int *n);
+
+void deleteStudent(Student students[], int *n);
+
+void updateStudent(Student students[], int n);
+
+void saveStudentsToFile(Student students[], int n);
+
+void loadStudentsFromFile(Student students[], int *n);
+
+void pressToExitOrReturn();
+
